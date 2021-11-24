616bbb1650237c170dba622c58e2e06bf9fee242
Luega <martinelliluca90@gmail.com>
2021-11-24 18:47:21 +0100

    some fun

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,13 +1,22 @@
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
+let numberOne = "";
+let numberTwo = "";
+let dieOne = document.getElementById("dieOne");
+let dieTwo = document.getElementById("dieTwo");
+let result = document.querySelector(".result");
 
-const numberOne = Math.floor(Math.random() * 6);
-const numberTwo = Math.floor(Math.random() * 6);
-if (numberOne > numberTwo) {
-    alert("Player won");
-} else if (numberOne < numberTwo) {
-    alert("PC won");
-} else {
-    alert("draw");
-}
- 
+document.getElementById("button").addEventListener("click", function() {
+    const numberOne = Math.floor(Math.random() * 6);
+    const numberTwo = Math.floor(Math.random() * 6);
+    dieOne.append(numberOne);
+    dieTwo.append(numberTwo);
+    if (numberOne > numberTwo) {
+        document.querySelector(".result").innerHTML = "Player won"
+    } else if (numberOne < numberTwo) {
+        document.querySelector(".result").innerHTML = "PC won"
+    } else {
+        document.querySelector(".result").innerHTML = "Draw"
+    }
+    result.classList.add("active");
+})
