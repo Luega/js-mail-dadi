0d683075f48c41847a51ce7f24c33ee66343b5d0
Luega <martinelliluca90@gmail.com>
2021-11-24 17:02:13 +0100

    logic

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,2 +1,13 @@
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+const numberOne = Math.floor(Math.random() * 6);
+const numberTwo = Math.floor(Math.random() * 6);
+if (numberOne > numberTwo) {
+    alert("Player won");
+} else if (numberOne < numberTwo) {
+    alert("PC won");
+} else {
+    alert("draw");
+}
+ 
