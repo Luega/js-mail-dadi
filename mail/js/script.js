af37e983a4869ebea4471c24b84349f760bd7185
Luega <martinelliluca90@gmail.com>
2021-11-24 16:35:37 +0100

    elimanted ;

--- mail/js/script.js
+++ mail/js/script.js
@@ -8,14 +8,14 @@ const email = prompt("Inserisci la tua email");
 let find = false;
 // array creation
 const accessEmailList = ["marta@gamil.com", "luca@gmail.com", "andrea@gmail.com", "irene@gmail.com", "franco@gmail.com"];
-// 
+// for cycle
 for (let i = 0; i < accessEmailList.length; i++) {
     if (email == accessEmailList[i]) {
         find = true;
-    };
-};
+    }
+}
 if (!find) {
     alert("ooooohhhhhh nooooo");
 } else {
     alert("yeah");
-};
+}
