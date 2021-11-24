af3f59a8168307dfa8a589970312fcccb2662b32
Luega <martinelliluca90@gmail.com>
2021-11-24 15:04:47 +0100

    added mail and array

---
+++ mail/js/script.js
@@ -0,0 +1,10 @@
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+
+
+// creation and assignment of variables
+const email = prompt("Inserisci la tua email");
+// array creation
+const accessEmailList = ["marta@gamil.com", "luca@gmail.com", "andrea@gmail.com", "irene@gmail.com", "franco@gmail.com"];
+// 
