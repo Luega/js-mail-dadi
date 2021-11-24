8b7463b55736da74acfa54c4ba6c23932c60040d
Luega <martinelliluca90@gmail.com>
2021-11-24 16:11:29 +0100

    added for

--- mail/js/script.js
+++ mail/js/script.js
@@ -5,6 +5,17 @@
 
 // creation and assignment of variables
 const email = prompt("Inserisci la tua email");
+let find = false;
 // array creation
 const accessEmailList = ["marta@gamil.com", "luca@gmail.com", "andrea@gmail.com", "irene@gmail.com", "franco@gmail.com"];
 // 
+for (let i = 0; i < accessEmailList.length; i++) {
+    if (email == accessEmailList[i]) {
+        find = true;
+    };
+};
+if (!find) {
+    alert("ooooohhhhhh nooooo");
+} else {
+    alert("yeah");
+};
